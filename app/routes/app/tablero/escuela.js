349b283cc7fcdf53a25f14142c2ef406ee7891af
Iván Hoffmann <digital.h@gmail.com>
2018-05-24 17:07:17 -0300

    Logos de herramientas. Avances en detalle de escuelas en tablero.

--- app/routes/app/tablero/escuela.js
+++ app/routes/app/tablero/escuela.js
@@ -1,4 +1,13 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Route.extend({
+  model(params) {
+    let escuela_id = params.escuela_id;
+    let escuela = this.store.findRecord("escuela", escuela_id);
+    let equipo = this.store.query("equipo", { escuela: escuela_id });
+    return {
+      escuela: escuela,
+      equipo: equipo
+    };
+  }
 });

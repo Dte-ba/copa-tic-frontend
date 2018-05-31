fc0390282afb0e61d477a676db43f94907791ef7
Iván Hoffmann <digital.h@gmail.com>
2018-05-31 15:59:42 -0300

    Bocetando Cabecera y footer oficial.

---
+++ app/components/header-cultura.js
@@ -0,0 +1,16 @@
+import Ember from "ember";
+
+export default Ember.Component.extend({
+  classNames: [
+    "ui",
+    "fixed",
+    "basic",
+    "right",
+    "aligned",
+    "inverted",
+    "bg",
+    "verde",
+    "segment",
+    "z-max"
+  ]
+});
--- app/routes/app/juego.js
+++ app/routes/app/juego.js
@@ -1,4 +1,28 @@
-import Ember from 'ember';
+import Ember from "ember";
+import { task } from "ember-concurrency";
+import ENV from "copa-tic-frontend/config/environment";
 
 export default Ember.Route.extend({
+  ajax: Ember.inject.service(),
+  obtenerActividades: task(function*() {
+    let query = {};
+    query.page_size = 100;
+
+    let data = yield this.store.query("escuela", query);
+    let meta = data.get("meta");
+    return { data, meta };
+  }).drop(),
+
+  escuelas: task(function*() {
+    let url = ENV.API_URL + "/api/actividades?page_size=100";
+    let resultado = yield this.get("ajax").request(url);
+    return resultado;
+  }).drop(),
+
+  model() {
+    return {
+      // listaDeActividades: this.get("obtenerActividades").perform()
+      listaDeActividades: this.get("escuelas").perform()
+    };
+  }
 });

d3ce0ee4dcf4e5b633ad52e35458774a2c1a153d
Iván Hoffmann <digital.h@gmail.com>
2018-06-14 16:07:56 -0300

    Ajustes de estilos varios en secciones. Header y Footer Final

--- app/routes/app/juego/index.js
+++ app/routes/app/juego/index.js
@@ -1,4 +1,12 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Route.extend({
+  model() {
+    return {
+      fase1: this.store.findRecord("fase", 1),
+      fase2: this.store.findRecord("fase", 2),
+      fase3: this.store.findRecord("fase", 3),
+      fase4: this.store.findRecord("fase", 4)
+    };
+  }
 });
